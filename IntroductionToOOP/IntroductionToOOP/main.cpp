d9285bc57128a18256e771f2ddc2e9835ecc507b
Clayman <Clayman@DESKTOP-DM8E77U>
2021-12-11 18:37:56 +0200

    Constructors.

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -18,7 +18,7 @@ public:
 	}
 	void set_x(double x)
 	{
-		this->x = x;
+		this->x = x;//Encapsulation DONE
 		//this - это указатель на объект, для которого вызывается метод
 		//Изнутри метода невозможно узнать имя объекта, для которого он вызывается
 		//НО всегда можно узнать адрес этого объекта.
@@ -27,7 +27,30 @@ public:
 	{
 		this->y = y;
 	}
-}A,B,C;
+
+	//					Constructors:
+	/*Point()
+	{
+		x = y = 0;
+		cout << "DefaultConstructor:\t" << this << endl;
+	}*/
+	Point(double x = 0, double y = 0)
+	{
+		this->x = x;
+		this->y = y;
+		cout << "Constructor:\t" << this << endl;
+	}
+	~Point()
+	{
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//				Methods:
+	void print()const
+	{
+		cout << "X = " << x << "\tY = " << y << endl;
+	}
+};
 
 //#define STRUCT_POINT
 
@@ -47,10 +70,22 @@ void main()
 	cout << pA->x << tab << pA->y << endl;
 #endif // STRUCT_POINT
 
-	Point A;
-	A.set_x(2);
-	A.set_y(3);
-	cout << A.get_x() << tab << A.get_y() << endl;
+	Point A;	//DefaultConstructor
+	double x, y;
+	//cout << "Введите координаты точки: "; cin >> x >> y;
+	//A.set_x(x);
+	//A.set_y(y);
+	//cout << A.get_x() << tab << A.get_y() << endl;
+	A.print();
+
+	Point B(22, 33);
+	B.print();
+
+	Point C = 5;	//Single-argument constructor
+	C.print();
+
+	Point D(0, 123);
+	D.print();
 }
 
 /*
@@ -61,19 +96,27 @@ void main()
 1. Инкапсуляция (Encapsulation);
 Модификаторы доступа:
 private:	доступны только внутри класса.
-public:		
+public:
 protected:
 get/set-методы
 
 get (взять, получить)	 - открывают доступ к переменным членам на чтение,
-						   т.е., поззволяют получить значение той или иной 
+						   т.е., поззволяют получить значение той или иной
 						   переменной-члена.
 set (задать, установить) - открывают доступ в переменным членам на запись,
-						   т.е., позволяют задать значение той или иной 
+						   т.е., позволяют задать значение той или иной
 						   переменной-члена.
 
 
 
 2. Наследование (Inheritance);
 3. Полиморфизм  (Polymorphism);
+*/
+
+/*
+				SPECIAL MEMBERS
+Constructor - это метод, который создает объект. А именно, выделяет память под объект,
+			  и инициализирует его переменные члены.
+~Destructor	-
+operator=
 */
--- ReadMe.txt
+++ ReadMe.txt
@@ -1,2 +1,7 @@
 https://github.com/okovtun/SPD_121.git
 https://www.youtube.com/watch?v=NItnq6ow4lg&list=PLeqyOOqxeiIPouwvOzxHWVhhwGBpv-FLJ
+
+TODO:
+1. Выучить теорию!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+2. Написать метод ??? distance(???), который возвращает расстояние до указанной точки;
+3. Написать функцию ??? distance(???), которая возвращает расстояние между двумя точками;
