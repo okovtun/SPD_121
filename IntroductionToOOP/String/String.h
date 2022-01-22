adfd4dc1461ebeb3aa4ef3cf9b1e39fbad6cb010
Clayman <Clayman@DESKTOP-DM8E77U>
2022-01-22 19:04:09 +0200

    Separation DONE.

--- IntroductionToOOP/String/String.h
+++ IntroductionToOOP/String/String.h
@@ -1,1 +1,49 @@
 #pragma once
+#pragma warning(disable:4326)
+
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;;
+
+int StringLength(const char* str);
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////		CLASS DECLARATION (ОБЪЯВЛЕНИЕ КЛАССА)			//////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+class String;
+String operator+(const String& left, const String& right);
+
+class String
+{
+	int size;	//Размер строки
+	char* str;	//Указатель на строку в динамической памяти
+public:
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
+
+	//					Constructors:
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	~String();
+
+	//					Operators:
+	String& operator=(const String& other);
+	String& operator+=(const String& other);
+
+	const char& operator[](int i)const;
+	char& operator[](int i);
+
+	//					Methods:
+	void print()const;
+};
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////	CLASS DECLARATION END (КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА)		//////////////
+//////////////////////////////////////////////////////////////////////////////////////
