206356e59556ba05468ee4115df1c3bc345e201c
Clayman <Clayman@DESKTOP-DM8E77U>
2022-01-22 18:57:10 +0200

    Methods out of class.

--- IntroductionToOOP/String/main.cpp
+++ IntroductionToOOP/String/main.cpp
@@ -10,6 +10,10 @@ using std::endl;;
 
 int StringLength(const char* str);
 
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////		CLASS DECLARATION (ОБЪЯВЛЕНИЕ КЛАССА)			//////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
 class String;
 String operator+(const String& left, const String& right);
 
@@ -18,80 +22,109 @@ class String
 	int size;	//Размер строки
 	char* str;	//Указатель на строку в динамической памяти
 public:
-	int get_size()const
-	{
-		return size;
-	}
-	const char* get_str()const
-	{
-		return str;
-	}
-	char* get_str()
-	{
-		return str;
-	}
+	int get_size()const;
+	const char* get_str()const;
+	char* get_str();
 
 	//					Constructors:
-	explicit String(int size = 80) :size(size), str(new char[size] {})
-	{
-		//Благодаря принимаемому параметру size мы можем создавать строки заданного размера
-		cout << "DefaultConst:\t" << this << endl;
-	}
-	String(const char* str) :String(strlen(str) + 1)
-	{
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t" << this << endl;
-	}
-	String(const String& other) :String(other.str)
-	{
-		//Глубокое копирование (Deep copy)
-		cout << "CopyConstructor:" << this << endl;
-	}
-	~String()
-	{
-		delete[] this->str;	//Если конструктор выделяет память при помощи оператора new,
-		//то деструктор обязательно должен освобождать эту память оператором delete[],
-		//в противном случае будет возникать утечка памяти.
-		cout << "Destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	~String();
 
 	//					Operators:
-	String& operator=(const String& other)
-	{
-		if (this == &other)return *this;
+	String& operator=(const String& other);
+	String& operator+=(const String& other);
 
-		int a = 2;
-		int b = 3;
-		a = b;
-		delete[] this->str;
-		//Глубокое копирование (Deep copy)
-		this->size = other.size;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
-		cout << "CopyAssignment:\t" << this << endl;
-		return *this;
-	}
-	String& operator+=(const String& other)
-	{
-		return *this = *this + other;
-	}
-
-	const char& operator[](int i)const
-	{
-		return str[i];
-	}
-	char& operator[](int i)
-	{
-		return str[i];
-	}
+	const char& operator[](int i)const;
+	char& operator[](int i);
 
 	//					Methods:
-	void print()const
-	{
-		cout << "Size:\t" << size << endl;
-		cout << "Str:\t" << str << endl;
-	}
+	void print()const;
 };
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////	CLASS DECLARATION END (КОНЕЦ ОБЪЯВЛЕНИЯ КЛАССА)		//////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+///--------------------------------------------------------------------------------///
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////		CLASS DEFINITION (ОПРЕДЕЛЕНИЕ КЛАССА)			//////////////
+//////////////////////////////////////////////////////////////////////////////////////
+int String::get_size()const
+{
+	return size;
+}
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+
+//					Constructors:
+String::String(int size) :size(size), str(new char[size] {})
+{
+	//Благодаря принимаемому параметру size мы можем создавать строки заданного размера
+	cout << "DefaultConst:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t" << this << endl;
+}
+String::String(const String& other) :String(other.str)
+{
+	//Глубокое копирование (Deep copy)
+	cout << "CopyConstructor:" << this << endl;
+}
+String::~String()
+{
+	delete[] this->str;	//Если конструктор выделяет память при помощи оператора new,
+	//то деструктор обязательно должен освобождать эту память оператором delete[],
+	//в противном случае будет возникать утечка памяти.
+	cout << "Destructor:\t" << this << endl;
+}
+
+//					Operators:
+String& String::operator=(const String& other)
+{
+	if (this == &other)return *this;
+
+	int a = 2;
+	int b = 3;
+	a = b;
+	delete[] this->str;
+	//Глубокое копирование (Deep copy)
+	this->size = other.size;
+	this->str = new char[size] {};
+	for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+	cout << "CopyAssignment:\t" << this << endl;
+	return *this;
+}
+String& String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+//					Methods:
+void String::print()const
+{
+	cout << "Size:\t" << size << endl;
+	cout << "Str:\t" << str << endl;
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -110,6 +143,10 @@ ostream& operator<<(ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
+
+//////////////////////////////////////////////////////////////////////////////////////
+////////////////	CLASS DEFINITION END (КОНЕЦ ОПРЕДЕЛЕНИЯ КЛАССА)		//////////////
+//////////////////////////////////////////////////////////////////////////////////////
 
 //#define NULL_TERMINATED_LINES
 //#define BASE_CHECK
