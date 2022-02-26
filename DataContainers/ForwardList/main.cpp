62a77d6f6c9ab4ee7758186f02d64544de7d79cf
Clayman <Clayman@DESKTOP-DM8E77U>
2022-02-26 15:25:01 +0200

    CountCorrect

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -27,10 +27,12 @@ unsigned int Element::count = 0;	//Инициализация статическ
 class ForwardList
 {
 	Element* Head;
+	unsigned int size;
 public:
 	ForwardList()
 	{
 		Head = nullptr;//Если Голова указывает на 0, то список пуст, т.е. не содежрит элементов
+		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
 	~ForwardList()
@@ -44,6 +46,7 @@ public:
 		Element* New = new Element(Data);
 		New->pNext = Head;
 		Head = New;
+		size++;
 	}
 	void push_back(int Data)
 	{
@@ -51,6 +54,7 @@ public:
 		Element* Temp = Head;
 		while (Temp->pNext)Temp = Temp->pNext;
 		Temp->pNext = new Element(Data);
+		size++;
 	}
 	void insert(int index, int Data)
 	{
@@ -64,6 +68,7 @@ public:
 		Element* New = new Element(Data);
 		New->pNext = Temp->pNext;
 		Temp->pNext = New;
+		size++;
 	}
 
 	//				Removing elements:
@@ -72,6 +77,7 @@ public:
 		Element* Erased = Head;
 		Head = Head->pNext;
 		delete Erased;
+		size--;
 	}
 	void pop_back()
 	{
@@ -79,6 +85,27 @@ public:
 		while (Temp->pNext->pNext)Temp = Temp->pNext;
 		delete Temp->pNext;
 		Temp->pNext = nullptr;
+		size--;
+	}
+	void erase(int index)
+	{
+		if (index == 0)return pop_front();
+		if (index == Head->count - 1)return pop_back();
+		if (index >= Head->count)
+		{
+			cout << "Error: out of range" << endl;
+			return;
+		}
+		//1) Доходим до нужного элемента:
+		Element* Temp = Head;
+		for (int i = 0; i < index - 1; i++)Temp = Temp->pNext;
+		//2) Запоминаем адрем удаляемого элемента:
+		Element* Erased = Temp->pNext;
+		//3) Исключаем удаляемый элемент из списка:
+		Temp->pNext = Temp->pNext->pNext;
+		//4) Удаляем элемент из памяти:
+		delete Erased;
+		size--;
 	}
 
 	//				Methods:
@@ -90,7 +117,8 @@ public:
 			cout << Temp << "\t" << Temp->Data << "\t" << Temp->pNext << endl;
 			Temp = Temp->pNext;
 		}
-		cout << "Количество элементов списка: " << Head->count << endl;
+		cout << "Количество элементов списка:" << size << endl;
+		cout << "Общее количество элементов: " << Head->count << endl;
 	}
 };
 
@@ -113,4 +141,16 @@ void main()
 	cout << "Введите значение добавляемого элемента: "; cin >> value;
 	list.insert(index, value);
 	list.print();
+
+	cout << "Введите индекс удаляемого элемента: "; cin >> index;
+	list.erase(index);
+	list.print();
+
+	ForwardList list2;
+	list2.push_back(123);
+	list2.push_back(456);
+	list2.push_back(789);
+
+	list.print();
+	list2.print();
 }
