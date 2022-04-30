ab4903680714e2ea000c5a252c1438072aa8fd9a
Oleg <31523835+okovtun@users.noreply.github.com>
2022-04-30 19:43:13 +0300

    Calc in progress

--- WinAPI/MainWindow/main.cpp
+++ WinAPI/MainWindow/main.cpp
@@ -3,13 +3,19 @@
 #include<cstdio>
 #include"resource.h"
 
-#define IDC_STATIC	1000	//Ресурс для static text
+//#define IDC_STATIC	1000	//Ресурс для static text
 
 CONST CHAR g_szClassName[] = "MyWindowClass";	//Имя класса окна
 CONST CHAR g_szWindowTitle[] = "My First Window";
 //g_ - Global
 //sz - string zero (NULL Terminated Line)
 //https://ru.wikipedia.org/wiki/%D0%92%D0%B5%D0%BD%D0%B3%D0%B5%D1%80%D1%81%D0%BA%D0%B0%D1%8F_%D0%BD%D0%BE%D1%82%D0%B0%D1%86%D0%B8%D1%8F
+
+CONST INT g_SCREEN_HEIGHT = 25;
+CONST INT g_iBtnSize = 50;	//Размер кнопки в пикселях
+CONST INT g_INTERVAL = 2;	//Интервал между кнопками в пикселях
+const int g_START_X = 10;
+const int g_START_Y = 10;
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -43,8 +49,12 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 
 	int screen_width = GetSystemMetrics(SM_CXSCREEN);
 	int screen_height = GetSystemMetrics(SM_CYSCREEN);
-	int window_width = screen_width * .75;
+	/*int window_width = screen_width * .75;
 	int window_height = screen_height * .75;
+	int start_x = screen_width * .125;
+	int start_y = screen_height * .125;*/
+	int window_width = g_START_X * 3 + (g_iBtnSize + g_INTERVAL) * 5 + g_INTERVAL;
+	int window_height = g_START_Y * 5 +g_SCREEN_HEIGHT+ (g_iBtnSize + g_INTERVAL) * 4 + g_INTERVAL*3;
 	int start_x = screen_width * .125;
 	int start_y = screen_height * .125;
 
@@ -53,7 +63,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, in
 		NULL,	//exStyle
 		g_szClassName,
 		g_szWindowTitle,
-		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX,// | WS_MAXIMIZEBOX,
+		WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU /*| WS_THICKFRAME*/ | WS_MINIMIZEBOX,// | WS_MAXIMIZEBOX,
 		//WS_OVERLAPPEDWINDOW,	//Главное окно, у которого есть строка заголовка, 
 		//кнопки управления окном, может изменяться по размерам....
 		start_x, start_y,	//Положение окна на экране
@@ -91,7 +101,8 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		//CreateWindowExA - ANSI-кодировка
 		//CreateWindowExW - Wide-кодировка (Unicode)
-		HWND hStatic = CreateWindowEx
+		//Create IDC_STATIC:
+		/*HWND hStatic = CreateWindowEx
 		(
 			0,
 			"Static",
@@ -104,10 +115,95 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			(HMENU)IDC_STATIC,
 			GetModuleHandle(NULL),	//GetModuleHandle(NULL) - Возвращает hInstance
 			0
+		);*/
+
+		CreateWindowEx
+		(
+			0, "Edit", "",
+			WS_BORDER | WS_CHILD | WS_VISIBLE | ES_RIGHT | ES_READONLY,
+			g_START_X, g_START_Y,
+			(g_iBtnSize + g_INTERVAL) * 5 - g_INTERVAL, g_SCREEN_HEIGHT,
+			hwnd, (HMENU)IDC_EDIT,
+			GetModuleHandle(NULL), NULL
+		);
+
+		int digit = 1;
+		char sz_digit[] = "0";
+		for (int i = 0; i < 3; i++)
+		{
+			for (int j = 0; j < 3; j++)
+			{
+				sz_digit[0] = '0' + digit;
+				CreateWindowEx
+				(
+					0, "Button", sz_digit,
+					WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+					g_START_X + (g_iBtnSize + g_INTERVAL)*j,
+					g_START_Y + g_SCREEN_HEIGHT + g_INTERVAL + (g_iBtnSize + g_INTERVAL)*(2 - i),
+					g_iBtnSize, g_iBtnSize,
+					hwnd, (HMENU)(IDC_BUTTON_0 + digit),
+					GetModuleHandle(NULL), NULL
+				);
+				digit++;
+			}
+		}
+
+		CreateWindowEx
+		(
+			0, "Button", "0",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X, g_START_Y + g_SCREEN_HEIGHT + (g_iBtnSize + g_INTERVAL) * 3,
+			g_iBtnSize * 2 + g_INTERVAL, g_iBtnSize,
+			hwnd, (HMENU)IDC_BUTTON_0,
+			GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			0, "Button", ".",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X + g_iBtnSize * 2 + g_INTERVAL * 2, g_START_Y + g_SCREEN_HEIGHT + (g_iBtnSize + g_INTERVAL) * 3,
+			g_iBtnSize, g_iBtnSize,
+			hwnd, (HMENU)IDC_BUTTON_POINT,
+			GetModuleHandle(NULL), NULL
+		);
+		CreateWindowEx
+		(
+			0, "Button", "C",
+			WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON,
+			g_START_X + (g_iBtnSize + g_INTERVAL) * 4, g_START_Y + g_SCREEN_HEIGHT + g_INTERVAL,
+			g_iBtnSize, g_iBtnSize * 2 + g_INTERVAL,
+			hwnd,
+			(HMENU)IDC_BUTTON_CLEAR,
+			GetModuleHandle(NULL),
+			NULL
 		);
 	}
 	break;
-	case WM_COMMAND:break;
+	case WM_COMMAND:
+	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		SendMessage(hEdit, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+		CHAR sz_digit[2] = {};
+		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
+		{
+			sz_digit[0] = LOWORD(wParam) - 952;
+			strcat(sz_buffer, sz_digit);
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_POINT)
+		{
+			if (strchr(sz_buffer, '.'))break;
+			strcat(sz_buffer, ".");
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
+		{
+			SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+		}
+	}
+	break;
 	case WM_SIZE:
 	case WM_MOVE:
 	{
@@ -123,7 +219,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		//https://www.cplusplus.com/reference/cstdio/sprintf/
 		//https://www.cplusplus.com/reference/cstdio/printf/
 		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_msg);
-		SendMessage(GetDlgItem(hwnd, IDC_STATIC), WM_SETTEXT, 0, (LPARAM)sz_msg);
+		//		SendMessage(GetDlgItem(hwnd, IDC_STATIC), WM_SETTEXT, 0, (LPARAM)sz_msg);
 	}
 	break;
 	case WM_CLOSE:
--- WinAPI/MainWindow/resource.h
+++ WinAPI/MainWindow/resource.h
@@ -6,6 +6,28 @@
 #define IDI_ICON_STAR                   102
 #define IDC_CURSOR1                     103
 
+#define IDC_EDIT			900
+
+#define IDC_BUTTON_0		1000
+#define IDC_BUTTON_1		1001
+#define IDC_BUTTON_2		1002
+#define IDC_BUTTON_3		1003
+#define IDC_BUTTON_4		1004
+#define IDC_BUTTON_5		1005
+#define IDC_BUTTON_6		1006
+#define IDC_BUTTON_7		1007
+#define IDC_BUTTON_8		1008
+#define IDC_BUTTON_9		1009
+
+#define IDC_BUTTON_PLUS		1043
+#define IDC_BUTTON_MINUS	1045
+#define IDC_BUTTON_ASTER	1042
+#define IDC_BUTTON_SLASH	1047
+#define IDC_BUTTON_EQUAL	1061
+#define IDC_BUTTON_POINT	1046
+#define IDC_BUTTON_CLEAR	1050
+
+
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
